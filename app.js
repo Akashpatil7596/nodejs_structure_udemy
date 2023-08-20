cbf026458defdf9b65000a50b13b557833798024
Akashpatil7596 <99664587+Akashpatil7596@users.noreply.github.com>
2023-08-20 09:56:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ import users from './services/v1/users/index.js'
 await mongo_connection('mongodb+srv://root:root@cluster0.u6ctlke.mongodb.net/?retryWrites=true&w=majority')
 
 app.use('/api', users)
-app.get('/',((req, res) => {
+app.get('/',(req, res) => {
     res.send('Yup I think This Works')
     }
 )
